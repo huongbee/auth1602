0d75b3fbd8c085a50cb32e6e7cb1b165d668353a
Huong <huongnguyenak96@gmail.com>
2019-05-12 11:16:23 +0700

    validate

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { UserService } from '../service/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -9,25 +10,53 @@ import { UserService } from '../service/user.service';
 })
 export class SignupComponent implements OnInit {
   signUpForm: FormGroup;
+  errorMessage: string;
 
-  constructor(private fb: FormBuilder, private userService: UserService) {
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) {
     this.signUpForm = this.fb.group({
       email: ['',
         [Validators.email, Validators.required]
       ],
       name: ['', Validators.required],
-      password: ['', Validators.required],
-      password_confirm: ['', Validators.required],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(6)
+      ]],
+      password_confirm: ['',
+        [
+          Validators.required,
+          Validators.minLength(6)
+        ]
+      ],
     });
   }
 
   ngOnInit() {
   }
   signUp() {
-    const { email, name, password } = this.signUpForm.value;
+    const { email, name, password, password_confirm } = this.signUpForm.value;
+    if (password !== password_confirm) {
+      return this.errorMessage = 'Password not match!';
+    }
     this.userService.signUp(email, name, password)
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
+    .then(res => {
+      if (res.code === 1) {
+        return this.router.navigateByUrl('/signin');
+      } else {
+        this.signUpForm.setValue({
+          email: '',
+          name: '',
+          password: '',
+          password_confirm: ''
+        });
+        this.errorMessage = res.message;
+      }
+    })
+    .catch(err => this.errorMessage = err.message);
   }
 
 }
