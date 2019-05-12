ee5dedc9dfc759a2365618d94ca56e0c7ba58d9d
Huong <huongnguyenak96@gmail.com>
2019-05-12 09:41:09 +0700

    ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
 import { ProfileComponent } from './profile/profile.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,9 @@ import { ProfileComponent } from './profile/profile.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  signUpForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.signUpForm = this.fb.group({
+      email: ['',
+        [Validators.email, Validators.required]
+      ],
+      name: ['', Validators.required],
+      password: ['', Validators.required],
+      password_confirm: ['', Validators.required],
+    });
+  }
 
   ngOnInit() {
   }
