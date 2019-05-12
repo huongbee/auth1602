e5b1e43b4a728b378f0f15ba8155a51def530f39
Huong <huongnguyenak96@gmail.com>
2019-05-12 09:29:26 +0700

    signup form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
