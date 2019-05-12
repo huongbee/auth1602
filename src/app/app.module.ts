15c9733783ddad152382dfd976db2d0a528a001a
Huong <huongnguyenak96@gmail.com>
2019-05-12 10:59:26 +0700

    Route[]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,20 +9,31 @@ import { ProfileComponent } from './profile/profile.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { UserService } from './service/user.service';
+import { RouterModule, Route } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Route[] = [
+    { path: '', component: ProfileComponent },
+    { path: 'signin', component: SigninComponent},
+    { path: 'signup', component: SignupComponent},
+    { path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
     SigninComponent,
-    ProfileComponent
+    ProfileComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
